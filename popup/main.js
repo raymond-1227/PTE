902cdbe54f48a1d26286d3fd7b234a7840e835cf
Raymond Hsu <40461364+raymond-1227@users.noreply.github.com>
2022-01-24 10:16:27 +0800

    Added localStorage usage

--- popup/main.js
+++ popup/main.js
@@ -1,15 +1,43 @@
-document.getElementById("toggle").onclick = function () {
-  if (this.checked == true) {
-    document.getElementById("toggleText").textContent = "ENABLED";
+const toggleStorageState = JSON.parse(
+  localStorage.getItem("toggleStorageState")
+);
+const check = document.getElementById("toggle");
+let currentToggleState = false;
+
+if (toggleStorageState) {
+  currentToggleState = toggleStorageState;
+  check.checked = currentToggleState;
+}
+
+if (currentToggleState == true) {
+  document.getElementById("toggleText").textContent = "ENABLED";
+} else {
+  document.getElementById("toggleText").textContent = "DISABLED";
+}
+
+check.onclick = function () {
+  if (this.checked) {
+    currentToggleState = true;
     browser.browserAction.setIcon({ path: "/icons/pte-32.png" });
     browser.tabs.executeScript({
       code: `document.designMode = "on"`,
     });
   } else {
-    document.getElementById("toggleText").textContent = "DISABLED";
+    currentToggleState = false;
     browser.browserAction.setIcon({ path: "/icons/pte-inactive-32.png" });
     browser.tabs.executeScript({
       code: `document.designMode = "off"`,
     });
   }
+  
+  if (currentToggleState == true) {
+    document.getElementById("toggleText").textContent = "ENABLED";
+  } else {
+    document.getElementById("toggleText").textContent = "DISABLED";
+  }
+
+  localStorage.setItem(
+    "toggleStorageState",
+    JSON.stringify(currentToggleState)
+  );
 };
